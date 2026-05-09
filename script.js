7bcc3a7e307568ead067b29dc53b336c32886764
Reyad Hossain <reyad4647@gmail.com>
2026-05-09 19:33:51 +0600

    first coomit

---
+++ script.js
@@ -0,0 +1,19 @@
+// Setup and start animation!
+        var typed = new Typed('#element', {
+            strings: ['Web Developer', 'Graphic Designer', 'Web Designer', 'Video Editor'],
+            typeSpeed: 50,
+        });
+
+function toggleMenu() {
+    document.getElementById("navLinks").classList.toggle("active");
+}
+
+function closeMenu() {
+    document.getElementById("navLinks").classList.remove("active");
+}   
+    
+    
+    
+
+
+

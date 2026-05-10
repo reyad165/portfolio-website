2841961c9c56291a36d7c5c4bb64103949091bc0
Reyad Hossain <reyad4647@gmail.com>
2026-05-10 23:25:58 +0600

    add portfolio index

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
+window.addEventListener("load", function () {
+  const loader = document.getElementById("loader-wrapper");
+  loader.style.display = "none";
+});
 // Setup and start animation!
-        var typed = new Typed('#element', {
-            strings: ['Web Developer', 'Graphic Designer', 'Web Designer', 'Video Editor'],
-            typeSpeed: 50,
-        });
+
+var typed = new Typed("#element", {
+  strings: [
+    "Web Developer",
+    "Graphic Designer",
+    "Web Designer",
+    "Video Editor",
+  ],
+  typeSpeed: 50,
+});
 
 function toggleMenu() {
-    document.getElementById("navLinks").classList.toggle("active");
+  document.getElementById("navLinks").classList.toggle("active");
 }
 
 function closeMenu() {
-    document.getElementById("navLinks").classList.remove("active");
-}   
-    
-    
-    
-
-
-
+  document.getElementById("navLinks").classList.remove("active");
+}
